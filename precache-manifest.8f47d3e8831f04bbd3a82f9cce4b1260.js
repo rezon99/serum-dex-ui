67983428f96886273085b8c1de9ab4694b2fe5c5
dr497 <anand.ratiney@gmail.com>
2021-04-22 00:47:02 +0800

    Updates

---
+++ precache-manifest.8f47d3e8831f04bbd3a82f9cce4b1260.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013be5ad54a154751fc1f9ff81c089fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acbaf0af9d0e7c847dd4",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "5ab03721dd213120db04",
+    "url": "/static/css/main.0d007200.chunk.css"
+  },
+  {
+    "revision": "acbaf0af9d0e7c847dd4",
+    "url": "/static/js/2.3ec2022e.chunk.js"
+  },
+  {
+    "revision": "d2e09aded9686c343e34a065031febca",
+    "url": "/static/js/2.3ec2022e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab03721dd213120db04",
+    "url": "/static/js/main.78b0c116.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
